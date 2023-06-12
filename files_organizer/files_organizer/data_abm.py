763b1071457e0e2150268f5cab8a0929d1b134f8
Matias Souto <masouto94@gmail.com>
2023-06-12 12:10:23 -0300

    Added decorator func. Implemented db

--- files_organizer/files_organizer/data_abm.py
+++ files_organizer/files_organizer/data_abm.py
@@ -7,6 +7,16 @@ class DatabaseConnector():
         self.cursor = self.connection.cursor()
         self.setup()
 
+    def _commit_if_success(self,func):
+        def container(*args):
+            try:
+                func(*args)
+            except sqlite3.Error as e:
+                print(f"{e}")
+                return
+        self.connection.commit()
+        return container
+
     def setup(self):
         self.cursor.execute("CREATE TABLE IF NOT EXISTS pairings(id INTEGER PRIMARY KEY, folder VARCHAR, extension VARCHAR UNIQUE)")
         self.connection.commit()
@@ -14,16 +24,48 @@ class DatabaseConnector():
     def select(self, query: str):
        return self.cursor.execute(query).fetchall()
 
-    def new_pairing(self, name: str, values: List):
-        data = [(name, value) for value in values]
+    # def new_pairing(self, name: str, value: str):
+    #         try:
+    #             self.cursor.execute(f"INSERT INTO pairings(folder,extension) VALUES(?,?)", (name,value))
+    #         except sqlite3.Error as e:
+    #             print(f"Could not save {name} {value}. {e}")
+    #             return
+    #         self.connection.commit()
+    #         print(f"Saved new pairing for {name}")
+
+    def new_pairing(self, name: str, value: str):
+        self._commit_if_success(
+                self.cursor.execute(f"INSERT INTO pairings(folder,extension) VALUES(?,?)", (name,value))
+                )
+        print(f"Saved new pairing for {name}")
+
+    def new_pairing_batch(self, name: str, values: List[str]):
         try:
-            self.cursor.executemany(f"INSERT INTO pairings(folder,extension) VALUES(?,?)", data)
+            data = [(name, value) for value in values]
+            query = self.cursor.executemany(f"INSERT INTO pairings(folder,extension) VALUES(?,?)", data)
         except sqlite3.Error as e:
             print(f"Could not save {name} {data}. {e}")
             return
         self.connection.commit()
-        print(f"Saved new pairing for {name}")
-    
+        print(f"Saved new pairings for {name} with {query.rowcount} elements")
 
+    def delete_pairing(self, name: str, value: str):
+        try:
+            self.cursor.execute(f"DELETE FROM pairings WHERE folder=? AND extension=?", (name,value))
+        except sqlite3.Error as e:
+            print(f"Could not delete {name} {value}. {e}")
+            return
+        self.connection.commit()
+        print(f"Deleted pairing {name} - {value}")
 
+    def delete_pairing_batch(self, name: str, values: List[str]):
+        try:
+           query = self.cursor.execute(f"DELETE FROM pairings WHERE folder=? AND extension in ({self._replace_placeholders(values)})", (name, *values))
+        except sqlite3.Error as e:
+            print(f"Could not delete {name} {values}. {e}")
+            return
+        self.connection.commit()
+        print(f"Deleted pairings for {name} with {query.rowcount} elements")
 
+    def _replace_placeholders(self, values:List[str]):
+        return ', '.join(map(lambda x: x.replace(x,'?'),values))
--- files_organizer/files_organizer/files_organizer.py
+++ files_organizer/files_organizer/files_organizer.py
@@ -1,7 +1,24 @@
 import os
-from functools import lru_cache
-from files_organizer.pairing import PAIRING
+import subprocess as sp
 
-@lru_cache
-def pair_files_by_type(dir):
-    pass
+
+def parse_data(data_pair):
+    pairing = {}
+    for pair in data_pair:
+        if pair[0] not in pairing.keys():
+            pairing[pair[0]] = []
+        pairing[pair[0]].append(pair[1])
+    return pairing
+
+def pair_files_by_type(dir, types):
+    files = [f"{dir}/{file}" for file in os.listdir(dir) if os.path.isfile(f"{dir}/{file}")]
+    folders = [f"{dir}/{folder}" for folder in types.keys() if not os.path.exists(f"{dir}/{folder}")]
+    if folders:
+        sp.run(["mkdir", *folders])
+    for type, extensions in types.items():
+        for file in files:
+            extension = os.path.splitext(file)[1]
+            if extension in extensions:
+                sp.run(["mv", file, f"{dir}/{type}/{os.path.basename(file)}"])
+
+
