0171de686c5113bf15ca274cf913e16993ed15a0
Matias Souto <masouto94@gmail.com>
2024-01-18 21:46:10 -0300

    redefine project

--- timeliner/app.py
+++ timeliner/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, send_file
 import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/hello")
+@app.route("/")
 def hello_world():
     data = pd.DataFrame(data={"lo":[1,2,3],"le":[9,3,1]})
-    return data.to_json()
+    data.to_csv('./data.csv')
+    
+    return send_file('./data.csv')
